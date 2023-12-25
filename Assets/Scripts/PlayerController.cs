525f6a3c3b6067301312e7fc4a6cf8d195c0da79
상빈 <67950060+17642@users.noreply.github.com>
2023-12-25 11:07:18 +0900

    4-3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float powerUpForce = 15.0f;
     public float speed = 5.0f;
     private Rigidbody playerRb;
     private GameObject focalPoint;
+    private bool hasPowerUp = false;
+    public GameObject powerupIndicator;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +22,34 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput); // 시점 방향으로 힘을 가함.
+        powerupIndicator.transform.position = transform.position+new Vector3(0,-0.5f,0);//PowerupIndicator 위치 지정
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerUpCountRoutine());
+            powerupIndicator.gameObject.SetActive(true);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+            Debug.Log("Collided With "+collision.gameObject.name+"With Powerup Set To"+hasPowerUp);
+            Rigidbody enemyRigidBody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position).normalized;//
+
+            enemyRigidBody.AddForce(awayFromPlayer*powerUpForce,ForceMode.Impulse);
+        }
+    }
+
+    IEnumerator PowerUpCountRoutine()
+    {
+        yield return new WaitForSeconds(7);//7초간 대기
+        hasPowerUp = false;//파워업 해제
+        powerupIndicator.gameObject.SetActive(false);
     }
 }
