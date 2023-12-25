1395d1f18c2e1607780df8577e669d46db8f4aab
상빈 <67950060+17642@users.noreply.github.com>
2023-12-25 11:50:29 +0900

    4-4
    Enemy Wave

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -20,5 +20,10 @@ public class Enemy : MonoBehaviour
     {
         Vector3 lookingDirection = (player.transform.position - transform.position).normalized;
         enemyRb.AddForce(lookingDirection * speed);//플레이어 방향으로 힘을 가함(플레이어 위치 - 현재 위치)*속력
+
+        if (transform.position.y < -10)
+        {
+            Destroy(gameObject);//y가 -10보다 낮으면(지형에서 떨어지면) 파괴
+        }
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,21 +5,34 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject powerUp;
+
+    public int enemyCount = 0;
 
     private float spawnRange = 9;
+
+    public int waveNumver = 1;
     // Start is called before the first frame update
     void Start()
     {
         //Instantiate(enemyPrefab, new Vector3(0, 0, 6),enemyPrefab.transform.rotation);//(0,0,6) 위치에 시범으로 적 생성
 
-        
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);//랜덤 위치에 적 생성
+
+        //Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);//랜덤 위치에 적 생성
+        SpawnEnemyWave(3);//웨이브 시작
+        Instantiate(powerUp, GenerateSpawnPosition(), powerUp.transform.rotation);//파워업 소환
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;//남은 Enemy 개체의 수
+        if (enemyCount == 0)
+        {
+            waveNumver++;
+            SpawnEnemyWave(waveNumver);//적이 0보다 적으면 적 추가 소환(다음 웨이브)
+            Instantiate(powerUp, GenerateSpawnPosition(), powerUp.transform.rotation);
+        }
     }
 
     Vector3 GenerateSpawnPosition()
@@ -30,5 +43,13 @@ public class SpawnManager : MonoBehaviour
         Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);//y는 0(땅에 붙어있어야 하므로)/ X와 Z는 랜덤 위치
 
         return randomPos;
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for(int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);//랜덤 위치 적 생성
+        }
     }
 }
